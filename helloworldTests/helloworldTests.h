6f5cf52f832fbf4dbe3118c6080eb56ec64f0f34
Arihant Jain <Arihant@Arihants-MacBook-Pro.local>
2012-08-06 09:29:12 -0400

    Initial Commit

---
+++ helloworldTests/helloworldTests.h
@@ -0,0 +1,13 @@
+//
+//  helloworldTests.h
+//  helloworldTests
+//
+//  Created by Arihant Jain on 8/6/12.
+//  Copyright (c) 2012 DMA. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface helloworldTests : SenTestCase
+
+@end
---
+++ helloworldTests/helloworldTests.m
@@ -0,0 +1,32 @@
+//
+//  helloworldTests.m
+//  helloworldTests
+//
+//  Created by Arihant Jain on 8/6/12.
+//  Copyright (c) 2012 DMA. All rights reserved.
+//
+
+#import "helloworldTests.h"
+
+@implementation helloworldTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in helloworldTests");
+}
+
+@end
